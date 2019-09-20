3f77aeacd22c5174e52fb8a2a6771fb364c8684c
User 1 <User 1@DDU-13-PC>
2019-09-20 11:22:03 +0530

    CASE_STUDY1

--- BasicModule_java/src/sample14/samp14.java
+++ BasicModule_java/src/sample14/samp14.java
@@ -12,7 +12,16 @@ public class samp14 {
 		int number=in.nextInt();
 		System.out.println("Enter the value of r");
 		int r=in.nextInt();
-		int ncr =0;
+		long n1=1,r1,n2=0,n3=1,a=0,i;
+		for(i = 1;i<=number;i++)
+		{
+			n1=n1*i;
+		}
+		for(i = 1;i<=number;i++)
+		{
+			r1=r1*1;
+		}
+		n2=
 		
 		System.out.println("Enter the number");
 		number=in.nextInt();
